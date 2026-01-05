7c820a288295f7b7ee81b2dad8a93e997f294274
Quilladi <helenestrm@yahoo.no>
2026-01-05 12:27:07 +0100

    lagt til panne for testing

---
+++ Assets/scripts/Pan.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Pan : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private float panSpeed = 0.1f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        transform.position = Vector2.Lerp(transform.position, mousePos, panSpeed);
+    }
+}
