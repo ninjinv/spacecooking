e933c5c83da8551a10c2cd902e24e85f69acdaea
Benjamin_Pet <56979864+BenjaminPet@users.noreply.github.com>
2026-01-05 14:09:57 +0100

    FoodLauncher Updates

--- Assets/scripts/Food.cs
+++ Assets/scripts/Food.cs
@@ -16,7 +16,7 @@ public class Food : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void LaunchFood()
--- Assets/scripts/FoodLauncher.cs
+++ Assets/scripts/FoodLauncher.cs
@@ -4,10 +4,13 @@ public class FoodLauncher : MonoBehaviour
 {
     public GameObject projectilePrefab;
 
+    private BoxCollider2D boxCollider;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        boxCollider = GetComponent<BoxCollider2D>();
         // projectilePrefab = gameObject;
         GameObject newProjectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
     }
@@ -15,8 +18,27 @@ public class FoodLauncher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnFood();
+        }
     }
 
+    void SpawnFood()
+    {
+        float AreaWidth = boxCollider.size.x;
+        Debug.Log(AreaWidth);
+        float AreaHight = boxCollider.size.y;
+        Debug.Log(AreaHight);
 
+        float SpawnAreaX = Random.Range(-AreaWidth/2, AreaWidth/2);
+        Debug.Log("randomNumX: " + SpawnAreaX);
+        float SpawnAreaY = Random.Range(-AreaHight/2, AreaHight/2);
+        Debug.Log("randomNumY: " + SpawnAreaY);
+
+        Vector2 spawnPosition = (Vector2)transform.position + new Vector2(SpawnAreaX, SpawnAreaY);
+        Instantiate(projectilePrefab, spawnPosition, transform.rotation);
+
+        Debug.Log($"Spawned projectile at {spawnPosition}");
+    }
 }
